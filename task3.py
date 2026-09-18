1ff62cc98cd92b88ca5b606ad5f094411a40c7dd
Mikalai Bohdan <2RoN4eG@gmail.com>
2026-09-18 23:08:23 +0300

    modified task3.py

--- task3.py
+++ task3.py
@@ -13,38 +13,50 @@
 """
 
 
-month = input("input month: ")
-year = int(input("input year: "))
+# month = input("input month: ")
+# year = int(input("input year: "))
 
-days = None
-match(month):
-    case 'January' | 'january':
-        days = 31
-    case 'February' | 'february' if year % 4 == 0:
-        days = 29
-    case 'February' | 'february':
-        days = 28 
-    case 'March' | 'march':
-        days = 31
-    case 'April' | 'april':
-        days = 30
-    case 'May' | 'may':
-        days = 31 
-    case 'June' | 'june':
-        days = 30 
-    case 'July' | 'july':
-        days = 31 
-    case 'August' | 'august':
-        days = 31
-    case 'September' | 'september':
-        days = 30
-    case 'October' | 'october':
-        days = 31
-    case 'November' | 'november':
-        days = 30
-    case 'December' | 'december':
-        days = 31
-    case _:
-        print("Не правильно задан месяц: месяц должен быть на английском")
+def get_days(month, year):
+    match(month.lower()):
+        case 'January' | 'january':
+            return 31
+        case 'February' | 'february' if year % 4 == 0 and year % 400 == 0 or year % 100 != 0:
+            return 29
+        case 'February' | 'february':
+            return 28 
+        case 'March' | 'march':
+            return 31
+        case 'April' | 'april':
+            return 30
+        case 'May' | 'may':
+            return 31 
+        case 'June' | 'june':
+            return 30 
+        case 'July' | 'july':
+            return 31 
+        case 'August' | 'august':
+            return 31
+        case 'September' | 'september':
+            return 30
+        case 'October' | 'october':
+            return 31
+        case 'November' | 'november':
+            return 30
+        case 'December' | 'december':
+            return 31
+        case _:
+            raise Exception("Не правильно задан месяц: месяц должен быть на английском")
 
-print("Результат: ", days)
+
+if __name__ == "__main__":
+    days = get_days("February", 2000)
+    print("Результат: ", days)
+
+    days = get_days("February", 2004)
+    print("Результат: ", days)
+
+    days = get_days("February", 2100)
+    print("Результат: ", days)
+
+    days = get_days("February", 1900)
+    print("Результат: ", days)
